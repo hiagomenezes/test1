47c584254b5c8c8db519f56042cf7a0ceca6a149
hiagomenezes <hiago@itecnologia.com.br>
2019-08-08 18:02:32 -0300

    teste para o programa carteirinha

---
+++ test1/Models/Times.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace test1.Models
+{
+    public class Times
+    {
+        [Display(Name = "Id")]
+        public int TimeId { get; set; }
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        public string Time { get; set; }
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        public string Estado { get; set; }
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        public string Cores { get; set; }
+    }
+}
